235dbc64d56cc1047e48869f9aa7c718fa0b6fd7
Sean Patrick Hagen <sean.hagen@gmail.com>
2018-12-04 13:26:02 -0800

    Day 2 part 2 done

---
+++ day2/part2/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/texttheater/golang-levenshtein/levenshtein"
+)
+
+/*
+
+--- Part Two ---
+Confident that your list of box IDs is complete, you're ready to find the boxes
+full of prototype fabric.
+
+The boxes will have IDs which differ by exactly one character at the same position
+in both strings. For example, given the following box IDs:
+
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+
+The IDs abcde and axcye are close, but they differ by two characters (the second and
+fourth). However, the IDs fghij and fguij differ by exactly one character, the third
+(h and u). Those must be the correct boxes.
+
+What letters are common between the two correct box IDs? (In the example above, this
+is found by removing the differing character from either ID, producing fgij.)
+
+*/
+
+func main() {
+	ids := []string{}
+	opts := levenshtein.Options{
+		InsCost: 2,
+		DelCost: 2,
+		SubCost: 1,
+		Matches: func(sourceCharacter rune, targetCharacter rune) bool {
+			return sourceCharacter == targetCharacter
+		},
+	}
+
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Printf("unable to open frequency file! reason: %v\n", err)
+		os.Exit(1)
+	}
+
+	r := bufio.NewReader(f)
+	line, _, err := r.ReadLine()
+	for ; err == nil; line, _, err = r.ReadLine() {
+		ids = append(ids, string(line))
+	}
+
+	var a, b string
+
+	for _, s := range ids {
+		for _, t := range ids {
+			if x := dist(s, t, opts); x == 1 {
+				a = s
+				b = t
+				goto done
+			}
+		}
+	}
+
+done:
+	fmt.Printf("two ids: \n\t%v\n\t%v\n\n", a, b)
+
+	both := same(a, b)
+	fmt.Printf("in both: \n\t%v\n\n", both)
+}
+
+func same(a, b string) string {
+	aparts := strings.Split(a, "")
+	bparts := strings.Split(b, "")
+
+	s := ""
+
+	for i, x := range aparts {
+		if x == bparts[i] {
+			s = fmt.Sprintf("%v%v", s, x)
+		}
+	}
+
+	return s
+}
+
+func dist(a, b string, opt levenshtein.Options) int {
+	if a == b {
+		return 100
+	}
+	return levenshtein.DistanceForStrings([]rune(a), []rune(b), opt)
+}
