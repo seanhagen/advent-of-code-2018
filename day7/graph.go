bea8bb1ff313907621c78b1fc0f4bc0b6ff0ef9d
Sean Patrick Hagen <sean.hagen@gmail.com>
2018-12-07 11:44:24 -0800

    Multiple start nodes are a thing

--- day7/graph.go
+++ day7/graph.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/seanhagen/advent-of-code-2018/lib"
@@ -14,27 +15,80 @@ import (
 
 // Node point in graph
 type Node struct {
-	name string
+	Name     string
+	Requires []*Node
+}
+
+// AddRequirement ...
+func (n *Node) AddRequirement(m *Node) {
+	n.Requires = append(n.Requires, m)
+}
+
+// MeetsRequirements ...
+func (n *Node) MeetsRequirements(r []*Node) bool {
+	allMet := make([]bool, len(n.Requires))
+
+	for i, n := range n.Requires {
+		for _, m := range r {
+			if n == m {
+				allMet[i] = true
+			}
+		}
+	}
+
+	met := true
+	for _, t := range allMet {
+		met = met && t
+	}
+	return met
+}
+
+// Test ...
+func (n *Node) Test() []string {
+	out := []string{}
+	for _, x := range n.Requires {
+		out = append(out, x.Name)
+	}
+	return out
 }
 
 // Graph data structure
 type Graph struct {
 	nodes map[string]*Node
+	edges map[*Node][]*Node
+}
+
+// Last ...
+func (g *Graph) Last() *Node {
+	for _, n := range g.nodes {
+		r := true
+		for m, _ := range g.edges {
+			if n == m {
+				r = false
+			}
+		}
+		if r {
+			return n
+		}
+	}
+
+	return nil
 }
 
 // Setup ...
 func (g *Graph) Setup(in *os.File) error {
 	g.nodes = map[string]*Node{}
+	g.edges = map[*Node][]*Node{}
 
 	err := lib.LoopOverLines(in, func(line []byte) error {
-		// fmt.Printf("%v\n", string(line))
 		bits := strings.Split(string(line), " ")
-		// spew.Dump(bits)
-
 		end := bits[1]
 		start := bits[7]
 
-		fmt.Printf("%v -> %v\n", start, end)
+		a := g.addNode(start)
+		b := g.addNode(end)
+		a.AddRequirement(b)
+		g.addEdge(b, a)
 
 		return nil
 	})
@@ -43,34 +97,123 @@ func (g *Graph) Setup(in *os.File) error {
 		return err
 	}
 
-	return err
+	return nil
+}
+
+// Children ...
+func (g Graph) Children(n *Node) []*Node {
+	out, ok := g.edges[n]
+	if !ok {
+		return []*Node{}
+	}
+	sort.Slice(out, func(i, j int) bool { return out[i].Name < out[j].Name })
+	return out
+}
+
+// FirstNode ...
+func (g Graph) FirstNodes() []*Node {
+	var first []*Node
+
+	areChildren := []*Node{}
+
+	for _, node := range g.nodes {
+		connections := g.edges[node]
+
+		for _, child := range connections {
+			add := true
+			for _, x := range areChildren {
+				if x == child {
+					add = false
+				}
+			}
+			if add {
+				areChildren = append(areChildren, child)
+			}
+		}
+	}
+
+	for _, node := range g.nodes {
+		// spew.Dump(node)
+		isChild := false
+		for _, n := range areChildren {
+			if n == node {
+				isChild = true
+			}
+		}
+		if !isChild {
+			first = append(first, node)
+		}
+	}
+	sort.Slice(first, func(i, j int) bool { return first[i].Name < first[j].Name })
+	return first
 }
 
 // Print ...
 func (g Graph) Print() string {
+	f := g.FirstNodes()
+
+	fmt.Printf("have %v nodes\n", len(g.nodes))
+
+	elements := []*Node{}
+	elements = append(elements, f...)
+
+	for i := 0; i < len(elements); i++ {
+		x := i + 1
+		n := elements[i]
+		children := g.Children(n)
+
+		for _, m := range children {
+			met := m.MeetsRequirements(elements[:x])
+			added := false
+
+			for _, z := range elements {
+				if m == z {
+					added = true
+				}
+			}
+
+			if met && !added {
+				elements = append(elements, &Node{})
+				copy(elements[x+1:], elements[x:])
+				elements[x] = m
+				x++
+			}
+		}
+	}
+
 	out := ""
+	for _, n := range elements {
+		out = fmt.Sprintf("%v%v", out, n.Name)
+	}
+
+	fmt.Printf("chain has %v elements\n", len(out))
 	return out
 }
 
 // AddNode ...
-func (g *Graph) addNode(a string) *Node {
-	n := g.findNode(a)
+func (g *Graph) addNode(a string, requires ...string) *Node {
+	n := g.FindNode(a)
 	if n != nil {
 		return n
 	}
 
-	n = &Node{a}
+	n = &Node{Name: a, Requires: []*Node{}}
 	g.nodes[a] = n
 	return n
 }
 
 // AddEdge ...
-func (g *Graph) addEdge(n, m string) {
-
+func (g *Graph) addEdge(from, to *Node) {
+	bits, ok := g.edges[from]
+	if !ok {
+		bits = []*Node{}
+	}
+	bits = append(bits, to)
+	g.edges[from] = bits
 }
 
-// findNode ...
-func (g Graph) findNode(f string) *Node {
+// FindNode ...
+func (g Graph) FindNode(f string) *Node {
 	n, ok := g.nodes[f]
 	if ok {
 		return n
--- day7/graph_test.go
+++ day7/graph_test.go
@@ -1,10 +1,66 @@
 package day7
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/seanhagen/advent-of-code-2018/lib"
 )
+
+func TestFirstNode(t *testing.T) {
+	f := lib.LoadInput("./fake.txt")
+	g := &Graph{}
+	err := g.Setup(f)
+	if err != nil {
+		t.Errorf("got error setting up graph: %v", err)
+		t.FailNow()
+	}
+
+	n := g.FirstNodes()
+	tmp := []string{}
+
+	for _, x := range n {
+		tmp = append(tmp, x.Name)
+	}
+	got := strings.Join(tmp, "")
+
+	expect := "C"
+
+	if n == nil {
+		t.Errorf("Expected a node, got nil")
+	} else {
+		if got != expect {
+			t.Errorf("Unexpected node, expected '%v', got '%v'", expect, got)
+		}
+	}
+}
+
+func TestFirstNodeChildren(t *testing.T) {
+	f := lib.LoadInput("./fake.txt")
+	g := &Graph{}
+	err := g.Setup(f)
+	if err != nil {
+		t.Errorf("got error setting up graph: %v", err)
+		t.FailNow()
+	}
+
+	x := g.FirstNodes()
+	n := x[0]
+
+	expect := "AF"
+
+	children := g.Children(n)
+
+	tmp := []string{}
+	for _, x := range children {
+		tmp = append(tmp, x.Name)
+	}
+	got := strings.Join(tmp, "")
+
+	if expect != got {
+		t.Errorf("didn't get proper children, expect '%v', got '%v'", expect, got)
+	}
+}
 
 func TestOutput(t *testing.T) {
 	f := lib.LoadInput("./fake.txt")
@@ -16,6 +72,23 @@ func TestOutput(t *testing.T) {
 	}
 
 	expect := "CABDFE"
+	got := g.Print()
+
+	if expect != got {
+		t.Errorf("wrong output, expected '%v', got '%v'", expect, got)
+	}
+}
+
+func TestMultipleStartNodes(t *testing.T) {
+	f := lib.LoadInput("./fake2.txt")
+	g := &Graph{}
+	err := g.Setup(f)
+	if err != nil {
+		t.Errorf("got error setting up graph: %v", err)
+		t.FailNow()
+	}
+
+	expect := "ABCED"
 	got := g.Print()
 
 	if expect != got {
