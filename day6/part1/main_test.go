2a316ed1c513ef7d5399b26de062f0f54cd4ae1d
Sean Patrick Hagen <sean.hagen@gmail.com>
2018-12-06 23:08:24 -0800

    Update tests some more

--- day6/part1/main_test.go
+++ day6/part1/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/seanhagen/advent-of-code-2018/lib"
@@ -64,4 +65,71 @@ bbb.ffffFf
 		t.Errorf("got bad board:\n expected:\n%v\n\ngot:\n%v\n\n", expect, got)
 	}
 
+}
+
+func TestInfinite(t *testing.T) {
+	should := []int{6, 1, 2, 3}
+
+	f := lib.LoadInput("../fake.txt")
+	b := &board{}
+	err := b.setup(f)
+	if err != nil {
+		t.Errorf("unable to setup board: %v", err)
+	}
+
+	b.compute()
+	inf := b.infinite()
+	got := []int{}
+	for _, p := range inf {
+		got = append(got, p)
+	}
+
+	if !arEq(should, got) {
+		t.Errorf("inf not equal. \nshould have %v\ngot %v", should, inf)
+	}
+}
+
+func TestFindLargest(t *testing.T) {
+	f := lib.LoadInput("../fake.txt")
+	b := &board{}
+	err := b.setup(f)
+	if err != nil {
+		t.Errorf("unable to setup board: %v", err)
+	}
+	b.compute()
+
+	should := 5
+	area := 17
+
+	foundID, highest := b.largestFinite()
+
+	if foundID != should {
+		t.Errorf("didn't find largest safe area, expected %v got %v", should, foundID)
+	}
+
+	if highest != area {
+		t.Errorf("found area not correct, expected %v got %v", area, highest)
+	}
+}
+
+func arEq(a, b []int) bool {
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+	sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+
+	// If one is nil, the other must also be nil.
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
