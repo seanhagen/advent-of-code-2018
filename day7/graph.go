849db33e85692f3cec05c7b43c95f3f839947e57
Sean Patrick Hagen <sean.hagen@gmail.com>
2018-12-07 01:13:45 -0800

    Working on graph thing

--- day7/graph.go
+++ day7/graph.go
@@ -1,18 +1,79 @@
 package day7
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
 
-type Node struct{}
+	"github.com/seanhagen/advent-of-code-2018/lib"
+)
 
-type Graph struct{}
+// https://flaviocopes.com/golang-data-structure-graph/
+// https://godoc.org/github.com/gonum/graph
+
+// Node point in graph
+type Node struct {
+	name string
+}
+
+// Graph data structure
+type Graph struct {
+	nodes map[string]*Node
+}
 
 // Setup ...
 func (g *Graph) Setup(in *os.File) error {
-	return nil
+	g.nodes = map[string]*Node{}
+
+	err := lib.LoopOverLines(in, func(line []byte) error {
+		// fmt.Printf("%v\n", string(line))
+		bits := strings.Split(string(line), " ")
+		// spew.Dump(bits)
+
+		end := bits[1]
+		start := bits[7]
+
+		fmt.Printf("%v -> %v\n", start, end)
+
+		return nil
+	})
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return err
 }
 
 // Print ...
 func (g Graph) Print() string {
 	out := ""
 	return out
+}
+
+// AddNode ...
+func (g *Graph) addNode(a string) *Node {
+	n := g.findNode(a)
+	if n != nil {
+		return n
+	}
+
+	n = &Node{a}
+	g.nodes[a] = n
+	return n
+}
+
+// AddEdge ...
+func (g *Graph) addEdge(n, m string) {
+
+}
+
+// findNode ...
+func (g Graph) findNode(f string) *Node {
+	n, ok := g.nodes[f]
+	if ok {
+		return n
+	}
+	return nil
 }
